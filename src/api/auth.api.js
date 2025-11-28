d278cb45f8713592fd3fbfa4ac19e62e5871773c
MaryFreiheit96 <mary.eiriz@gmail.com>
2025-11-28 18:13:14 +0100

    add signup module

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -5,3 +5,7 @@ const BASE_PATH = '/auth'
 export const login = async data => {
   return httpClient.post(`${BASE_PATH}/login`, data)
 }
+
+export const signup = async data => {
+  return httpClient.post(`${BASE_PATH}/signup`, data)
+}
